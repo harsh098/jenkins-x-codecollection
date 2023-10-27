b77f1f552413dcddb3d2991e2a2b7ab34633b6d1
Harsh Mishra <hmisraji07@gmail.com>
2023-10-27 07:47:50 +0000

    Fixed Formatting for Readability

--- codebundles/jx_logs/runbook.robot
+++ codebundles/jx_logs/runbook.robot
@@ -9,6 +9,7 @@ Library             RW.Core
 Library             RW.CLI
 Library             OperatingSystem
 Library             JXKeywords.Pipelines.PipelineInfo
+
 Suite Setup         Suite Initialization
 
 
@@ -16,16 +17,16 @@ Suite Setup         Suite Initialization
 Get Logs of Latest Build
     [Documentation]    Fetches logs for the latest Jenkins X Build
     [Tags]    jenkinsx pipeline log logs
+    # ...    target_service=${kubectl}
     ${pipeline}=    RW.CLI.Run Cli
     ...    cmd=${KUBERNETES_DISTRIBUTION_BINARY} get pipelineruns --no-headers -n ${NAMESPACE} --sort-by='.metadata.creationTimestamp' --context=${CONTEXT} | awk '{print $1}' | tail -n 1
-    # ...    target_service=${kubectl}
     ...    env=${env}
     ...    secret_file__kubeconfig=${kubeconfig}
     ...    include_in_history=false
 
+    # ...    target_service=${kubectl}
     ${logs}=    RW.CLI.Run Cli
     ...    cmd=for pod in $( ${KUBERNETES_DISTRIBUTION_BINARY} get pipelineruns $(${KUBERNETES_DISTRIBUTION_BINARY} get pipelineruns --no-headers -n ${NAMESPACE} --sort-by='.metadata.creationTimestamp' --context=${CONTEXT} | awk '{print $1}' | tail -n 1) -n ${NAMESPACE} -o json --context=${CONTEXT} | jq -r '.status.taskRuns| .[] | .status.podName');do ${KUBERNETES_DISTRIBUTION_BINARY} get pod $pod -n ${NAMESPACE} --context=${CONTEXT} &>/dev/null && ${KUBERNETES_DISTRIBUTION_BINARY} logs pod/$pod -n ${NAMESPACE} --all-containers --ignore-errors --context=${CONTEXT} || echo -e "Containers Exited"; done
-    # ...    target_service=${kubectl}
     ...    env=${env}
     ...    secret_file__kubeconfig=${kubeconfig}
     ...    render_in_commandlist=true
@@ -34,8 +35,6 @@ Get Logs of Latest Build
     RW.Core.Add Pre To Report
     ...    Recent logs from JenkinsX Pipeline Run ${pipeline.stdout} in ${NAMESPACE}:\n\n${logs.stdout}
     RW.Core.Add Pre To Report    Commands Used:\n ${history}
-
-
 
 Get Logs of Failing Builds
     [Documentation]    Fetches Logs of Failing Steps in Failing Builds
@@ -50,10 +49,12 @@ Get Logs of Failing Builds
     ${time_total}=    Convert To Integer    ${TIME_INTERVAL}
     ${time_days}=    Evaluate    ${time_total}//86400
     ${time_hours}=    Evaluate    (${time_total}%86400)//3600
-    ${time_minutes}=   Evaluate    ((${time_total}%86400)%3600)//60
+    ${time_minutes}=    Evaluate    ((${time_total}%86400)%3600)//60
     ${time_seconds}=    Evaluate    ((${time_total}%86400)%3600)%60
-    RW.Core.Add Pre To Report    Failing Pipeline Runs in Last ${time_days} Days, ${time_hours} Hours, ${time_minutes} Minutes, ${time_seconds} Seconds
+    RW.Core.Add Pre To Report
+    ...    Failing Pipeline Runs in Last ${time_days} Days, ${time_hours} Hours, ${time_minutes} Minutes, ${time_seconds} Seconds
     RW.Core.Add Pre To Report    ${report}
+
 
 *** Keywords ***
 Suite Initialization
@@ -63,8 +64,8 @@ Suite Initialization
     ...    description=The kubernetes kubeconfig yaml containing connection configuration used to connect to cluster(s).
     ...    pattern=\w*
     ...    example=For examples, start here https://kubernetes.io/docs/concepts/configuration/organize-cluster-access-kubeconfig/
-    
-    #  TODO: Add support for dedicated CLI services in future
+
+    #    TODO: Add support for dedicated CLI services in future
     # ${kubectl}=    RW.Core.Import Service    kubectl
     # ...    description=The location service used to interpret shell commands.
     # ...    default=kubectl-service.shared
@@ -81,29 +82,29 @@ Suite Initialization
     ...    description=Which Kubernetes context to operate within.
     ...    pattern=\w*
     ...    example=my-main-cluster
- 
+
     ${KUBERNETES_DISTRIBUTION_BINARY}=    RW.Core.Import User Variable    KUBERNETES_DISTRIBUTION_BINARY
     ...    type=string
     ...    description=Which binary to use for Kubernetes CLI commands.
     ...    enum=[kubectl,oc]
     ...    example=kubectl
     ...    default=kubectl
-    
+
     ${TIME_INTERVAL}=    RW.Core.Import User Variable    TIME_INTERVAL
     ...    type=string
     ...    description=Time interval (seconds) to measure in Range 1 to 604800 both included
     ...    pattern=^(?!0\d*$)(?![7-9]\d{5}$)\d{1,6}$
     ...    example=3600
     ...    default=86400
-    
-    ${TEKTON_API_VERSION}=    RW.Core.Import User Variable   TEKTON_API_VERSION
+
+    ${TEKTON_API_VERSION}=    RW.Core.Import User Variable    TEKTON_API_VERSION
     ...    type=string
     ...    description=API Version for use in Tekton over JenkinsX.
     ...    pattern=\w*
     ...    example=v1beta1
     ...    default=v1beta1
 
-    #  TODO: Add Support for jx cli binary in future tasks
+    #    TODO: Add Support for jx cli binary in future tasks
     # ${JX_BINARY}=    RW.Core.Import User Variable    JX_BINARY
     # ...    type=string
     # ...    description=Path to jx binary for JX CLI Commands
--- libraries/JXKeywords/Pipelines/PipelineInfo.py
+++ libraries/JXKeywords/Pipelines/PipelineInfo.py
@@ -9,6 +9,7 @@ from datetime import datetime, timedelta
 KUBECONFIG = os.getenv("KUBECONFIG", default="")
 logger = logging.getLogger(__name__)
 
+
 class PipelineRun:
     def __init__(self, kubeconfig=KUBECONFIG, tektonVersion="v1beta1", namespace="jx", context="sandbox-cluster-1"):
         self._kubeconfig = kubeconfig
@@ -52,7 +53,14 @@ class PipelineRun:
                 "tekton.dev", version=self.tektonVersion, namespace=self._namespace, plural="pipelineruns", **kwargs
             )["items"]
 
-            response = [(run["metadata"]["name"], run["status"]["conditions"][0]["status"], run["metadata"]["creationTimestamp"]) for run in pipelineRuns]
+            response = [
+                (
+                    run["metadata"]["name"],
+                    run["status"]["conditions"][0]["status"],
+                    run["metadata"]["creationTimestamp"],
+                )
+                for run in pipelineRuns
+            ]
 
             return response
 
@@ -68,7 +76,11 @@ class PipelineRun:
             if len(pipelineRuns) == 0:
                 return []
             response = [
-                (run["metadata"]["name"], run["status"]["conditions"][0]["status"], run["metadata"]["creationTimestamp"])
+                (
+                    run["metadata"]["name"],
+                    run["status"]["conditions"][0]["status"],
+                    run["metadata"]["creationTimestamp"],
+                )
                 for run in pipelineRuns
                 if run["status"]["conditions"][0]["status"] == "False"
             ]
@@ -77,19 +89,32 @@ class PipelineRun:
 
         except ApiException as Error:
             return []
-        
+
     def get_build_pods_and_steps_from_pipeline_run_name(self, pipelineRunName: str):
         try:
-            pipelineRunWrapper = self.customApi.get_namespaced_custom_object("tekton.dev", version=self.tektonVersion, namespace=self._namespace, plural="pipelineruns", name=pipelineRunName)
+            pipelineRunWrapper = self.customApi.get_namespaced_custom_object(
+                "tekton.dev",
+                version=self.tektonVersion,
+                namespace=self._namespace,
+                plural="pipelineruns",
+                name=pipelineRunName,
+            )
             taskRuns = pipelineRunWrapper["status"]["taskRuns"]
-            response = [(taskRun["pipelineTaskName"], taskRun["status"]["podName"], taskRun["status"]["steps"]) for _,taskRun in taskRuns.items()]
+            response = [
+                (taskRun["pipelineTaskName"], taskRun["status"]["podName"], taskRun["status"]["steps"])
+                for _, taskRun in taskRuns.items()
+            ]
             return response
         except ApiException as Error:
             return None
 
 
 def sli_for_pipeline_runs(
-    kubeconfig=KUBECONFIG, namespace:str="jx", context:str="sandbox-cluster-1", tektonVersion:str="v1beta1", timeInterval:str="86400"
+    kubeconfig=KUBECONFIG,
+    namespace: str = "jx",
+    context: str = "sandbox-cluster-1",
+    tektonVersion: str = "v1beta1",
+    timeInterval: str = "86400",
 ) -> Tuple:
     global logger
     kubeconfig_location = kubeconfig
@@ -107,20 +132,20 @@ def sli_for_pipeline_runs(
         kubeconfig_location = f"./{kubeconfig.key}"
 
     x_seconds_ago = datetime.now() - timedelta(seconds=timeInterval)
-    
 
     PipelineRunObject = PipelineRun(
         kubeconfig=kubeconfig_location, tektonVersion=tektonVersion, context=context, namespace=namespace
     )
 
     total_runs = [
-        item for item in PipelineRunObject.get_pipeline_runs()
+        item
+        for item in PipelineRunObject.get_pipeline_runs()
         if datetime.strptime(item[2], "%Y-%m-%dT%H:%M:%SZ") >= x_seconds_ago
     ]
     failed_runs = [
-        item for item in PipelineRunObject.get_failed_pipeline_runs()
+        item
+        for item in PipelineRunObject.get_failed_pipeline_runs()
         if datetime.strptime(item[2], "%Y-%m-%dT%H:%M:%SZ") >= x_seconds_ago
- 
     ]
     total_pipeline_runs = len(total_runs)
     failed_pipeline_runs = len(failed_runs)
@@ -132,31 +157,33 @@ def sli_for_pipeline_runs(
     return (total_pipeline_runs, failed_pipeline_runs)
 
 
-
-def _get_pod_logs(podName:str, container:str, kubeconfig=KUBECONFIG, namespace:str="jx", context:str="sandbox-cluster-1", env:dict=None):
-    cmd = ''.join(
+def _get_pod_logs(
+    podName: str,
+    container: str,
+    kubeconfig=KUBECONFIG,
+    namespace: str = "jx",
+    context: str = "sandbox-cluster-1",
+    env: dict = None,
+):
+    cmd = "".join(
         [
             "${KUBERNETES_DISTRIBUTION_BINARY}  ",
-            f" --context={context} logs -n {namespace} pod/{podName} -c {container} 2> /dev/null || echo 'Containers Exited'"
+            f" --context={context} logs -n {namespace} pod/{podName} -c {container} 2> /dev/null || echo 'Containers Exited'",
         ]
     )
-    output:str
+    output: str
     if type(kubeconfig) == platform.Secret:
-        output = run_cli(
-            cmd = cmd,
-            env = env,
-            secret_file__kubeconfig=kubeconfig
-        )
+        output = run_cli(cmd=cmd, env=env, secret_file__kubeconfig=kubeconfig)
     else:
         output = run_cli(
-            cmd = cmd,
-            env = env,
+            cmd=cmd,
+            env=env,
         )
-    
+
     return output.stdout
 
+
 def _generate_report(fail_report):
-    
     body = []
 
     for taskRun in fail_report["taskRuns"]:
@@ -171,7 +198,7 @@ def _generate_report(fail_report):
                 -----------------------------------------------------
             """
             steps.append(stepObject)
-        stepsReport= "\n".join(steps)
+        stepsReport = "\n".join(steps)
         taskReport = f"""
             Task Run Name : {taskRun["taskRunName"]}
             Logging Pod : {taskRun["podName"]}
@@ -180,10 +207,9 @@ def _generate_report(fail_report):
             {stepsReport} 
         """
         body.append(taskReport)
-    
 
     response_body = "\n".join(body)
-    report=f"""
+    report = f"""
         ________________________________________________________________
         Pipeline Run : {fail_report["failedPipelineRunName"]}
         Creation Timestamp: {fail_report["creationTimestamp"]}
@@ -194,7 +220,14 @@ def _generate_report(fail_report):
     return report
 
 
-def get_failing_steps_in_failed_builds(kubeconfig=KUBECONFIG, namespace:str="jx", context:str="sandbox-cluster-1", tektonVersion:str="v1beta1", timeInterval:str="86400", env:dict=None):
+def get_failing_steps_in_failed_builds(
+    kubeconfig=KUBECONFIG,
+    namespace: str = "jx",
+    context: str = "sandbox-cluster-1",
+    tektonVersion: str = "v1beta1",
+    timeInterval: str = "86400",
+    env: dict = None,
+):
     global logger
     kubeconfig_location = kubeconfig
 
@@ -217,48 +250,57 @@ def get_failing_steps_in_failed_builds(kubeconfig=KUBECONFIG, namespace:str="jx"
     )
 
     failed_runs = [
-        item for item in PipelineRunObject.get_failed_pipeline_runs()
+        item
+        for item in PipelineRunObject.get_failed_pipeline_runs()
         if datetime.strptime(item[2], "%Y-%m-%dT%H:%M:%SZ") >= x_seconds_ago
     ]
 
     failed_steps = []
     for run in failed_runs:
-        failed_run_name =  run[0]
-        failed_timestamp =  run[2]
-        build_pods_and_steps_from_pipeline_run_name=PipelineRunObject.get_build_pods_and_steps_from_pipeline_run_name(failed_run_name)
+        failed_run_name = run[0]
+        failed_timestamp = run[2]
+        build_pods_and_steps_from_pipeline_run_name = PipelineRunObject.get_build_pods_and_steps_from_pipeline_run_name(
+            failed_run_name
+        )
         run_failed_steps = []
         try:
             for taskName, podName, steps in build_pods_and_steps_from_pipeline_run_name:
-                task_failed_steps = [{
-                    "name": step["name"], 
-                    "containerName": step["container"] , 
-                    "logs": _get_pod_logs(podName=podName, container=step["container"], kubeconfig=kubeconfig_location, namespace=namespace, env=env, context=context)
-                    } for step in steps if step["terminated"]["exitCode"]!=0]
-                info = {
-                    "taskRunName" : taskName,
-                    "podName" : podName,
-                    "failedSteps" : task_failed_steps
-                }
+                task_failed_steps = [
+                    {
+                        "name": step["name"],
+                        "containerName": step["container"],
+                        "logs": _get_pod_logs(
+                            podName=podName,
+                            container=step["container"],
+                            kubeconfig=kubeconfig_location,
+                            namespace=namespace,
+                            env=env,
+                            context=context,
+                        ),
+                    }
+                    for step in steps
+                    if step["terminated"]["exitCode"] != 0
+                ]
+                info = {"taskRunName": taskName, "podName": podName, "failedSteps": task_failed_steps}
                 run_failed_steps.append(info)
             info = {
-                "failedPipelineRunName" : failed_run_name,
-                "creationTimestamp" : failed_timestamp,
-                "taskRuns" : run_failed_steps
+                "failedPipelineRunName": failed_run_name,
+                "creationTimestamp": failed_timestamp,
+                "taskRuns": run_failed_steps,
             }
             failed_steps.append(info)
         except KeyError as k:
             # Skipping Pending pipelines as of noe
-            # TODO: Add support for Failed pipelines with Pending Status 
+            # TODO: Add support for Failed pipelines with Pending Status
             continue
-        
+
     if type(kubeconfig) == platform.Secret:
         os.remove(f"./{kubeconfig.key}")
-    
+
     reports = []
     for step in failed_steps:
         reports.append(_generate_report(step))
-    
-    final_report = '\n'.join(reports)
 
+    final_report = "\n".join(reports)
 
     return final_report
